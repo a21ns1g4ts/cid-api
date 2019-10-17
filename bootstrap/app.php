4fc62befdf3ed8b5f2919be24abfa086736f4225
Atiladanvi <atila.danvi@outlook.com>
2019-10-17 20:43:45 -0300

    Restaura pastas do framework

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -58,6 +58,27 @@ $app->bind(
     \App\Repositories\Cid10RepositoryContract::class,
     \App\Repositories\Cid10FileRepository::class
 );
+
+/*
+|--------------------------------------------------------------------------
+| Register Container Bindings
+|--------------------------------------------------------------------------
+|
+| Now we will register a few bindings in the service container. We will
+| register the exception handler and the console kernel. You may add
+| your own bindings here if you like or you can make another file.
+|
+*/
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
 /*
 |--------------------------------------------------------------------------
 | Register Middleware
